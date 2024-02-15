df3ccd6f7f247a80ce9fba7ff6da10a30cd97750
Fabrizio Musacchio <fabrizio.musacchio@posteo.de>
2024-02-15 23:12:12 +0100

    Updating main code

--- ann_with_numpy.py
+++ ann_with_numpy.py
@@ -1,3 +1,19 @@
+"""
+A Python script to implement a simple neural network using NumPy.
+
+author: Fabrizio Musacchio
+date: Feb 15, 2024
+
+For reproducibility:
+
+conda create -n numpy_ann python=3.11
+conda activate numpy_ann
+conda install -y mamba
+mamba install -y numpy matplotlib keras ipykernel
+
+If you are using an Apple Silicon chip, follow these instructions to install TensorFlow:
+https://www.fabriziomusacchio.com/blog/2022-11-10-apple_silicon_and_tensorflow/
+"""
 # %% IMPORTS
 import numpy as np
 import matplotlib.pyplot as plt
@@ -31,7 +47,7 @@ def sigmoid(z):
     return 1 / (1 + np.exp(-z))
 
 # plot the sigmoid function:
-fig = plt.figure(figsize=(6, 3))
+fig = plt.figure(figsize=(4, 3))
 z = np.linspace(-10, 10, 100)
 plt.plot(z, sigmoid(z))
 plt.xlabel('z')
@@ -71,7 +87,7 @@ def compute_cost(a2, Y):
     """
     We use the cross-entropy loss function to compute the loss.
     """
-    m = Y.shape[0]  # Anzahl der Beispiele
+    m = Y.shape[0]  # number of examples
     log_probs = np.multiply(np.log(a2), Y) + np.multiply((1 - Y), np.log(1 - a2))
     cost = -np.sum(log_probs) / m
     return cost
@@ -117,124 +133,12 @@ def update_parameters(network, gradients, learning_rate):
     network['W2'] -= learning_rate * gradients['dW2']
     network['b2'] -= learning_rate * gradients['db2']
     
-def train_network(network, X, Y, num_iterations=1000, learning_rate=0.01):
-    """
-    This function trains the neural network using the given input data X and the true labels Y.
-    It works as follows:
-    - It performs the feedforward step to compute the output.
-    - It computes the loss using the output and the true labels.
-    - It performs the backpropagation step to compute the gradients.
-    - It updates the parameters of the network using the gradients and the learning rate.
-    - It returns the loss for each iteration (for plotting the learning curve later).
-    """
-    costs = []
-    for i in range(num_iterations):
-        activations = feedforward(network, X)
-        cost = compute_cost(activations['a2'], Y)
-        costs.append(cost)
-        gradients = backpropagate(network, activations, X, Y)
-        update_parameters(network, gradients, learning_rate)
-        
-        if i % 100 == 0:
-            print("Kosten nach Iteration %i: %f" %(i, cost))
-    return costs
-# %% LOAD AND PREPROCESSING THE DATA
-
-# load MNIST handwritten digit data:
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
-
-# display some images:
-fig, axes = plt.subplots(ncols=5, sharex=False,
-			 sharey=True, figsize=(10, 4))
-for i in range(5):
-	axes[i].set_title(y_train[i])
-	axes[i].imshow(X_train[i], cmap='gray')
-	axes[i].get_xaxis().set_visible(False)
-	axes[i].get_yaxis().set_visible(False)
-plt.show()
-
-# repeat the plot for only 9 images and arange them in a 3x3 grid:
-fig, axes = plt.subplots(nrows=3, ncols=3, figsize=(10, 10))
-for i, ax in enumerate(axes.flat):
-	ax.imshow(X_train[i], cmap='gray')
-	ax.set_title(f"true label: {y_train[i]}")
-	ax.axis('off')
-plt.show()
-
-# flatten the input data and normalize:
-X_train_flat = X_train.reshape(X_train.shape[0], -1) / 255.
-X_test_flat  = X_test.reshape(X_test.shape[0], -1) / 255.
-
-# convert the labels to "one-hot" format
-def convert_to_one_hot(Y, C):
-    """
-    "one-hote" encoding of the labels, i.e., the labels are represented as binary vectors.
-    E.g., the label 3 is represented as [0, 0, 0, 1, 0, 0, 0, 0, 0, 0]. This is required 
-    for the output layer of the network, as it requires a binary vector for each label.
-    """
-    Y = np.eye(C)[Y.reshape(-1)]
-    return Y
-
-num_classes = 10  # Anzahl der Klassen im MNIST-Datensatz
-y_train_one_hot = convert_to_one_hot(y_train, num_classes)
-y_test_one_hot = convert_to_one_hot(y_test, num_classes)
-# %% TRAINING THE NETWORK
-""" # initialize the network:
-network = initialize_network(input_size, hidden_size, output_size) """
-
-# train the network:
-epochs = 1000 # here, epochs equals the number of iterations since we use the entire dataset for each iteration (no mini-batch)
-learning_rate = 0.5
-loss_history = train_network(network, X_train_flat, y_train_one_hot, 
-                             num_iterations=epochs, learning_rate=learning_rate)
-
-""" Note: These parameters (especially the learning rate and the number of iterations) 
-can be adjusted to improve the performance of the network. In practice, this requires 
-some experimentation. """
-# %% PREDICTION AND EVALUATION
-# define the predict function:
-def predict(network, X):
+def train_network(network, X_train, Y_train, X_val, Y_val, num_iterations=1000, learning_rate=0.1):
     """
     We predict the labels for the input data X using the trained network:
     - We perform the feedforward step to compute the output.
-    - We take the class (digits 0-9) with the highest probability as the predicted label    
+    - We take the class (digits 0-9) with the highest probability as the predicted label.
     """
-    activations = feedforward(network, X)
-    predictions = np.argmax(activations['a2'], axis=1)
-    return predictions
-
-# predict the test data:
-predictions = predict(network, X_test_flat)
-
-# extract the actual labels from the "one-hot" vectors:
-actual_labels = y_test # np.argmax(y_test, axis=1)
-
-# calculate the accuracy:
-accuracy = np.mean(predictions == actual_labels)
-print(f'Accuracy of the network on the test data: {accuracy * 100:.2f}%')
-
-# Display some predictions on test data
-fig, axes = plt.subplots(ncols=10, sharex=False,
-			 sharey=True, figsize=(20, 4))
-for i in range(10):
-	axes[i].set_title(predictions[i])
-	axes[i].imshow(X_test[i], cmap='gray')
-	axes[i].get_xaxis().set_visible(False)
-	axes[i].get_yaxis().set_visible(False)
-plt.show()
-
-# plot the loss over epochs:
-fig = plt.figure(figsize=(10, 6))
-plt.plot(loss_history)
-plt.xlabel('epoch')
-plt.ylabel('Loss')
-plt.title('Loss curve over epochs')
-plt.show
-# %% TRAINING AND VALIDATION THE NETWORK
-# reset the network:
-network = initialize_network(input_size, hidden_size, output_size)
-
-def train_network(network, X_train, Y_train, X_val, Y_val, num_iterations=epochs, learning_rate=learning_rate):
     train_costs = []
     val_costs = []
     for i in range(num_iterations):
@@ -254,8 +158,57 @@ def train_network(network, X_train, Y_train, X_val, Y_val, num_iterations=epochs
             print(f"Costs after iteration {i}: Training {train_cost}, Validation {val_cost}")
     return train_costs, val_costs
 
-epochs = 1000 
+# define the predict function:
+def predict(network, X):
+    """
+    We predict the labels for the input data X using the trained network:
+    - We perform the feedforward step to compute the output.
+    - We take the class (digits 0-9) with the highest probability as the predicted label    
+    """
+    activations = feedforward(network, X)
+    predictions = np.argmax(activations['a2'], axis=1)
+    return predictions
+# %% LOAD AND PREPROCESSING THE DATA
+
+# load MNIST handwritten digit data:
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+# display some images:
+fig, axes = plt.subplots(nrows=3, ncols=3, figsize=(10, 10))
+for i, ax in enumerate(axes.flat):
+	ax.imshow(X_train[i], cmap='gray')
+	ax.set_title(f"true label: {y_train[i]}")
+	ax.axis('off')
+plt.tight_layout()
+plt.savefig('mnist_images.png', dpi=300)
+plt.show()
+
+# flatten the input data and normalize:
+X_train_flat = X_train.reshape(X_train.shape[0], -1) / 255.
+X_test_flat  = X_test.reshape(X_test.shape[0], -1) / 255.
+
+# convert the labels to "one-hot" format
+def convert_to_one_hot(Y, C):
+    """
+    "one-hote" encoding of the labels, i.e., the labels are represented as binary vectors.
+    E.g., the label 3 is represented as [0, 0, 0, 1, 0, 0, 0, 0, 0, 0]. This is required 
+    for the output layer of the network, as it requires a binary vector for each label.
+    """
+    Y = np.eye(C)[Y.reshape(-1)]
+    return Y
+
+num_classes = 10  #  number of classes (digits 0-9) in the MNIST dataset
+y_train_one_hot = convert_to_one_hot(y_train, num_classes)
+y_test_one_hot  = convert_to_one_hot(y_test, num_classes)
+# %% TRAINING AND VALIDATION THE NETWORK
+""" # reset the network:
+network = initialize_network(input_size, hidden_size, output_size) """
+
+# set the hyperparameters:
+epochs = 1000 # here, epochs equals the number of iterations since we use the entire dataset for each iteration (no mini-batch)
 learning_rate = 0.5
+
+# train the network:
 train_costs, val_costs = train_network(network, X_train_flat, y_train_one_hot, X_test_flat, y_test_one_hot, 
                                        num_iterations=epochs, learning_rate=learning_rate)
 
@@ -280,23 +233,24 @@ plt.gca().spines['top'].set_visible(False)
 plt.gca().spines['right'].set_visible(False)
 plt.legend()
 plt.tight_layout()
+plt.savefig('training_validation_loss.png', dpi=300)
 plt.show()
 
 # predict the test data:
 predictions = predict(network, X_test_flat)
 
-# extract the actual labels from the "one-hot" vectors:
-actual_labels = y_test # np.argmax(y_test, axis=1)
-
 # calculate the accuracy:
+actual_labels = y_test
 accuracy = np.mean(predictions == actual_labels)
 print(f'Accuracy of the network on the test data: {accuracy * 100:.2f}%')
 
-# repeat the plot for only 9 images and arange them in a 3x3 grid:
+# plot some test images with the predicted labels:
 fig, axes = plt.subplots(nrows=3, ncols=3, figsize=(10, 10))
 for i, ax in enumerate(axes.flat):
 	ax.imshow(X_test[i], cmap='gray')
 	ax.set_title(f'predicted label: {predictions[i]}')
 	ax.axis('off')
+plt.tight_layout()
+plt.savefig('predicted_labels.png', dpi=300)
 plt.show()
 # %% END
